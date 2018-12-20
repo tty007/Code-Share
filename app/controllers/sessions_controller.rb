d8e1b77b0beab1521ac18029294e9f3d09e74902
tty007 <c7dc007@gmail.com>
2018-12-20 14:13:46 +0900

    implement twitter-login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_or_create_from_auth(auth_hash)
+    session[:user_id] = user.id
+    flash[:notice] = "ユーザー認証が完了しました。"
+    redirect_to root_path
+  end
+
+  def destroy
+    reset_session
+    flash[:notice] = "ログアウトしました。"
+    redirect_to root_path
+  end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+end
