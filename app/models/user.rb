5974a3f83a1802266ed196b35103dee0e988c386
tty007 <c7dc007@gmail.com>
2018-12-20 13:39:31 +0900

    create User model

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  def self.find_or_create_from_auth(auth)
+    provider = auth[:provider]
+    uid = auth[:uid]
+    user_name = auth[:info][:user_name]
+    image_url = auth[:info][:image]
+
+    self.find_or_create_by(provider: provider, uid: uid) do |user|
+      user.user_name = user_name
+      user.image_url = image_url
+    end
+  end
+end
