4195d1e14486ec11434b86917803a95a12e52832
tty007 <c7dc007@gmail.com>
2018-12-22 01:01:51 +0900

    add a static file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  root to: 'home#index'
+  get '/about', to: 'home#about'
+  get 'contact', to: 'home#contact'
+  get 'howtouse', to: 'home#howto'
   get '/profile', to: 'users#show'
   resources :codes
   #Twitter用のルーティング
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
-  root to: 'home#index'
+  
 end
