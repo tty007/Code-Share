3e0d66a5d11bd79be70042f2bca278e770ad0873
tty007 <c7dc007@gmail.com>
2018-12-21 12:43:30 +0900

    implement update_attributes processing

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,11 @@ class User < ApplicationRecord
     image_url = auth[:info][:image]
     user_desc = auth[:info][:description]
 
+    # 該当ユーザ有りなら値を更新する
+    if e_user = self.find_by(uid: uid)
+      e_user.update_attributes(user_name: user_name, image_url: image_url, description: user_desc)
+    end
+
     #該当ユーザ無しなら新規に作成する
     self.find_or_create_by(provider: provider, uid: uid) do |user|
       user.user_name = user_name
