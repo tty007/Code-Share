b6c46d02a4f409ff4893ea24d593d057ec3165b0
tty007 <c7dc007@gmail.com>
2018-12-22 09:00:43 +0900

    stick navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  #固定ページ設定
   root to: 'home#index'
   get '/about', to: 'home#about'
   get 'contact', to: 'home#contact'
   get 'howtouse', to: 'home#howto'
+
+  #ユーザープロフィール
   get '/profile', to: 'users#show'
+  #Codeモデルに対するリソースルート生成
   resources :codes
   #Twitter用のルーティング
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
-  
 end
