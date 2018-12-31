01f1130a142f4aa6aa27631eb4001bc962d2e9e2
tty007 <c7dc007@gmail.com>
2018-12-31 14:54:50 +0900

    add delete comment button

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,20 @@
 class CommentsController < ApplicationController
     def create
-        @code = Code.friendly.find(params[:code_id])
-        @comment = @code.comments.build(comment_params)
-        @comment.user_id = current_user.id
-        if @comment.save
-          redirect_to code_path(@code)
-        else
-          render "code/show"
-        end
+      @code = Code.friendly.find(params[:code_id])
+      @comment = @code.comments.build(comment_params)
+      @comment.user_id = current_user.id
+      if @comment.save
+        redirect_to code_path(@code)
+      else
+        render "code/show"
+      end
+    end
+
+    def destroy
+      @comment = current_user.comments.find(params[:id])
+      @comment.destroy
+      flash[:notice] = 'コメントを削除しました'
+      redirect_to code_path(id: @comment.code_id)
     end
 
     private
