b484a29f75763783675c7ba923a904f196abaf57
tty007 <c7dc007@gmail.com>
2019-02-01 16:29:14 +0900

    refact

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -49,18 +49,15 @@ class CodesController < ApplicationController
     @code = current_user.codes.friendly.find(params[:id])
     # @codeに紐づいた画像も一緒に削除
     if @code.image_url.present?
-      #ファイルが存在すれば削除
       if FileTest.exists?("#{Rails.root}/public/ogp/#{@code.image_url}")
         FileUtils.rm("#{Rails.root}/public/ogp/#{@code.image_url}")
         @code.image_url = nil
       else
-        #なければnilを格納のみ
         @code.image_url = nil
       end
     end
 
     @code.update_attributes(code_params)
-    # 新たに画像を生成
     create_ogp(@code)    
 
     if @code.update(code_params)
@@ -76,12 +73,10 @@ class CodesController < ApplicationController
 
     # @codeに紐づいた画像も一緒に削除
     if @code.image_url.present?
-      #ファイルが存在すれば削除
       if FileTest.exists?("#{Rails.root}/public/ogp/#{@code.image_url}")
         FileUtils.rm("#{Rails.root}/public/ogp/#{@code.image_url}")
       end
     end
-    #@codeインスタンスを削除する
     @code.destroy
     redirect_to :action => 'index'
   end
@@ -92,59 +87,40 @@ class CodesController < ApplicationController
     params.require(:code).permit(%i(filename description body))
   end
 
-   #/./public/以下を""に切り取る
-   def cut_path(url)
+  def cut_path(url)
     url.sub(/\.\/public\/ogp\//, "")
   end
 
-  #.piblic/ogp/ランダムな文字列.pngというファイル名に加工し、そのPATHを返す
   def uniq_file_name
     "./public/ogp/#{SecureRandom.hex}.png"
   end
 
-  #20文字ごとに改行を入れる
   def cut_text(text)
       text.scan(/.{1,20}/).join("\n")
   end
 
   def create_ogp(code)
-    #テキストを書き込みための画像を読み込む
     image = Magick::ImageList.new('./public/images/twitter-ogp.png')
-    #画像に線や文字を描画するDrawオブジェクトの生成
     draw = Magick::Draw.new
-    #cut_textの処理結果をtitleに代入
     title = cut_text(code.filename)
 
-    #文字の描画（引数は、画像、幅、高さ、X座標、Y座標、描画する文字）
     draw.annotate(image, 0, 0, 0, -10, title) do
-      #日本語対応可能なフォントにする(rootから読み込み)
       self.font = "#{Rails.root}/.fonts/ipag.ttf"
-      #フォントの塗りつぶし色
       self.fill = '#fff'
-      #描画基準位置(中央)
       self.gravity = Magick::CenterGravity
-      #フォントの太さ
       self.font_weight = Magick::BoldWeight
-      #フォント縁取り色(透過)
       self.stroke = 'transparent'
-      #フォントサイズ
       self.pointsize = 38
     end
-
-    #下に定義したuniq_file_nameメソッドの処理結果のファイル名をimage_pathに代入
     image_path = image.write(uniq_file_name).filename
-    #下に定義したcut_textメソッド処理の結果をimage_urlを代入
     image_url = cut_path(image_path)
-    #@codeに作成画像であるimage_urlを追加
     code.image_url = image_url
   end
 
-  # コードにいいねする
   def like(user)
     likes.create(user_id: user.id)
   end
 
-  # コードのいいねを解除する
   def unlike(user)
     likes.find_by(user_id: user.id).destroy
   end
