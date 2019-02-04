03a77f83afadc4ab1b1f8d536452da7a5f12bfa5
tty007 <c7dc007@gmail.com>
2019-02-04 10:32:35 +0900

    implement html2canvas

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -1,17 +1,11 @@
 class CodesController < ApplicationController
-  # ファイルを削除するためのライブラリ
-  require "fileutils"
 
   def index
     @codes = Code.all
   end
 
   def show
-    #frendly_idで実装する
     @code = Code.friendly.find(params[:id])
-    #コメントインスタンスを生成
-    # @comment = @code.comments.build
-    # [.png]を削除したurlを取得するためのURL
     if @code.image_url.present?
       @image_url = @code.image_url.sub(/\.png/, '')
     end
@@ -23,10 +17,7 @@ class CodesController < ApplicationController
 
   def create
     @code = current_user.codes.build(code_params)
-    #uuidを生成する
     @code.uuid = SecureRandom.uuid
-    create_ogp(@code)
-
     if @code.save
       flash[:notice] = "コードを作成しました"
       redirect_to codes_url
@@ -42,19 +33,7 @@ class CodesController < ApplicationController
 
   def update
     @code = current_user.codes.friendly.find(params[:id])
-    # @codeに紐づいた画像も一緒に削除
-    if @code.image_url.present?
-      if FileTest.exists?("#{Rails.root}/public/ogp/#{@code.image_url}")
-        FileUtils.rm("#{Rails.root}/public/ogp/#{@code.image_url}")
-        @code.image_url = nil
-      else
-        @code.image_url = nil
-      end
-    end
-
-    @code.update_attributes(code_params)
-    create_ogp(@code)    
-
+    @code.update_attributes(code_params)   
     if @code.update(code_params)
       redirect_to code_url(@code)
     else
@@ -65,26 +44,16 @@ class CodesController < ApplicationController
   def destroy
     flash[:notice] = 'コードファイルを削除しました。'
     @code = current_user.codes.friendly.find(params[:id])
-
-    # @codeに紐づいた画像も一緒に削除
-    if @code.image_url.present?
-      if FileTest.exists?("#{Rails.root}/public/ogp/#{@code.image_url}")
-        FileUtils.rm("#{Rails.root}/public/ogp/#{@code.image_url}")
-      end
-    end
     @code.destroy
     redirect_to :action => 'index'
   end
 
 
   def ranking
-    #groupメソッドでcode_idが同じものをグループに分ける
-    #order('count(id) desc')で、idの数でオーダーを降順でつける（つまり多い順から）
-    #pluckでそのカラムのみの情報を取り出し値を返す
     @codes = Code.find(Like.group(:code_id).order('count(code_id) desc').limit(20).pluck(:code_id))
   end
 
-  private
+    private
 
   def code_params
     params.require(:code).permit(%i(filename description body))
@@ -92,32 +61,6 @@ class CodesController < ApplicationController
 
   def cut_path(url)
     url.sub(/\.\/public\/ogp\//, "")
-  end
-
-  def uniq_file_name
-    "./public/ogp/#{SecureRandom.hex}.png"
-  end
-
-  def cut_text(text)
-      text.scan(/.{1,20}/).join("\n")
-  end
-
-  def create_ogp(code)
-    image = Magick::ImageList.new('./public/images/twitter-ogp.png')
-    draw = Magick::Draw.new
-    title = cut_text(code.filename)
-
-    draw.annotate(image, 0, 0, 0, -10, title) do
-      self.font = "#{Rails.root}/.fonts/ipag.ttf"
-      self.fill = '#fff'
-      self.gravity = Magick::CenterGravity
-      self.font_weight = Magick::BoldWeight
-      self.stroke = 'transparent'
-      self.pointsize = 38
-    end
-    image_path = image.write(uniq_file_name).filename
-    image_url = cut_path(image_path)
-    code.image_url = image_url
   end
 
   def like(user)
